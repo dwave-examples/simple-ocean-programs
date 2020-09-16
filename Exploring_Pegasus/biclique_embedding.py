c0f9a5bc740a0e2ed3f8eefda98ea76067165195
Joel M. Gottlieb <jgottlieb@dwavesys.com>
2020-09-16 11:44:40 -0400

    Initial version

---
+++ Exploring_Pegasus/biclique_embedding.py
@@ -0,0 +1,57 @@
+# Copyright 2020 D-Wave Systems Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+import networkx as nx
+import dwave_networkx as dnx
+from minorminer import find_embedding
+from dwave.system.samplers import DWaveSampler
+
+# Form the biclique
+N = int(sys.argv[1])
+nodes1 = np.arange(N)
+nodes2 = [chr(i + ord('a')) for i in range(N)]
+B = nx.Graph()
+B.add_nodes_from(nodes1)
+B.add_nodes_from(nodes2)
+for node1 in nodes1:
+    for node2 in nodes2:
+        B.add_edge(node1, node2)
+
+fig, axes = plt.subplots(nrows=1, ncols=3, figsize=(12, 6))
+
+# Draw the QUBO as a networkx graph
+pos = nx.spring_layout(B)
+nx.draw_networkx(B, pos=pos, font_size=10, node_size=100, node_color='cyan', ax=axes[0])
+
+# Embed the graph on Chimera
+dwave_sampler_chimera = DWaveSampler(solver={'topology__type__eq': 'chimera'})
+A_chimera = dwave_sampler_chimera.edgelist
+chimera_graph = dnx.chimera_graph(16, edge_list=A_chimera)
+embedding_chimera = find_embedding(B, chimera_graph)
+
+# Draw the graph embedded on Chimera
+dnx.draw_chimera_embedding(chimera_graph, embedding_chimera, embedded_graph=B, unused_color=None, ax=axes[1])
+
+# Embed the graph on Pegasus
+dwave_sampler_pegasus = DWaveSampler(solver={'topology__type__eq': 'pegasus'})
+A_pegasus = dwave_sampler_pegasus.edgelist
+pegasus_graph = dnx.pegasus_graph(16, edge_list=A_pegasus)
+embedding_pegasus = find_embedding(B, pegasus_graph)
+
+# Draw the graph embedded on Pegasus
+dnx.draw_pegasus_embedding(pegasus_graph, embedding_pegasus, embedded_graph=B, unused_color=None, ax=axes[2])
+plt.savefig('biclique_embedding')
