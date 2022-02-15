1c09b8d7cb42e3a3ef84d5bd6dfb050d047304e5
jwarrell <jonathan.warrell@gmail.com>
2022-02-15 23:25:36 +0000

    Merge pull request #18 from hhtong/skipint
    Skip integration test when running weekly tests

---
+++ Basic_Programs/new_ising.py
@@ -0,0 +1,24 @@
+from dwave.system import EmbeddingComposite, FixedEmbeddingComposite, DWaveSampler
+
+import random
+
+#h = {}
+#J = {('A','K'): -0.5,
+#    ('B','C'): -0.5, 
+#    ('A','C'): 0.5}
+
+h = {1: 0.2,
+    2: -0.2}
+J = {(1,2): -0.5,
+    (3,4): -0.5, 
+    (1,4): 0.5}    
+
+# Define the sampler that will be used to run the problem
+sampler = EmbeddingComposite(DWaveSampler())
+
+# Run the problem on the sampler and print the results
+sampleset = sampler.sample_ising(h, J,
+                                 num_reads = 10,
+                                 label='Example - Simple Ocean Programs: Ising')
+print(sampleset)
+
